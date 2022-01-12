c06a51747fd44a08027534c8b92fbdcfa2555496
Alberto Campos <alberto.campos@heyirys.com>
2022-01-12 12:06:07 -0600

    added useState example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+
+  const add = () => {
+    setCounter((current) => current + 1);
+  };
+  const decrease = () => {
+    setCounter((current) => current - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Name: {name}</p>
+        <p>Count {counter}</p>
+        <input type="text" onChange={(event) => setName(event.target.value)} />
+        <button onClick={add}>+</button>
+        <button onClick={decrease}>-</button>
       </header>
     </div>
   );
